0d9680a0796769a136be6f1a94de9618445693c0
Erico Vinicius <48500205+ericovinicosta@users.noreply.github.com>
2023-03-02 13:56:48 -0300

    envio via vscode do github

---
+++ l1q11.py
@@ -0,0 +1,22 @@
+""" Faça um Programa que peça 2 números inteiros e um número real.
+     Calcule e mostre:
+    ◦ o produto do dobro do primeiro com metade do segundo.
+    ◦ a soma do triplo do primeiro com o terceiro.
+    ◦ o terceiro elevado ao cubo."""
+
+#entrada
+
+numero_1 = int(input("Digite o 1o número inteiro: "))
+numero_2 = int(input("Digite o 2o número inteiro: "))
+numero_real = float(input("Digite um número real: "))
+
+#processamento
+calculo_1 = numero_1 * 2 * numero_2 / 2
+calculo_2 = 3 * numero_1 + numero_real
+calculo_3 = numero_real * numero_real * numero_real
+
+#saída
+
+print("o produto do dobro do primeiro com metade do segundo. {}").format(calculo_1)
+print("a soma do triplo do primeiro com o terceiro. {}").format(calculo_2)
+print("o terceiro elevado ao cubo. {}").format(calculo_3)
